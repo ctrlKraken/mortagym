bbd94bff8167f77737b6605fc36ceca895662f91
ctrlKraken <ctrlkraken.404@gmail.com>
2025-09-30 18:15:47 -0300

    Header+Footer v1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import { RiFacebookBoxFill, RiInstagramFill } from '@remixicon/react';
 
-export default function Footer() {
+function Footer() {
   return (
-    <footer className="bg-gray-100 py-6">
-      <div className="container mx-auto text-center">
-        <p>© {new Date().getFullYear()} Morta Gym — Síguenos: Instagram · Facebook</p>
-      </div>
-    </footer>
-  )
+    <>
+      <section id="cta" className="cta">
+        <div className="container" data-aos="zoom-in">
+          <div className="text-center">
+            <p> ¡Seguinos en nuestras redes!</p>
+            <div className="social-links mt-3">
+              <a href="https://www.facebook.com" className="facebook" target="_blank" style={{ marginRight: "10px" }}> <RiFacebookBoxFill size={40} /> </a>
+              <a href="https://www.instagram.com" className="instagram" target="_blank" style={{ marginRight: "20px" }}><RiInstagramFill size={40} /></a>
+            </div>
+          </div>
+        </div>
+      </section>
+      <footer id="footer" className="text-center py-2">
+        <p className='copy'>CTRL Kraken © {new Date().getFullYear()}</p>
+      </footer>
+    </>
+  );
 }
+
+export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,31 @@
-// src/components/Navbar.jsx
 import { NavLink, Link } from 'react-router-dom'
+import logo from '../assets/logo_sf.png'
 
-export default function Navbar(){
+export default function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container">
-        <Link className="navbar-brand" to="/">Morta Gym</Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNav">
+    <nav className="navbar navbar-expand-lg custom-navbar shadow-sm">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">
+          <img
+            src={logo}
+            alt="Morta Gym logo"
+            height="85"     
+            className="me-2"
+          />
+          Morta Gym</Link>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#mainNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
+
         <div className="collapse navbar-collapse" id="mainNav">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item"><NavLink className="nav-link" to="/">Inicio</NavLink></li>
